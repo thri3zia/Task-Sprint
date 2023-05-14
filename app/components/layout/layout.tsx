10fa570d0c9dfb9b2f551b84874648ed41fc010f
David Bryan Pael <PAEL20229184@MLHUILLIER.LOCAL>
2023-05-15 07:42:05 +0800

    [FIXED] Response callback

--- app/components/layout/layout.tsx
+++ app/components/layout/layout.tsx
@@ -2,22 +2,21 @@
 
 import { useState } from "react";
 import AddImage from "../addImage";
-import AddTag from "./addtag";
-import SubmitButton from "./button";
 import MediaBox from "./media";
 import ProductInformation from "./productInfo";
 import SalesInformation from "./salesInformation";
 import Shipping from "./shipping";
 import TagSection from "./tagsection";
-import { Product, Sales } from "@/app/models/product";
+import { Product, Sales, Shipping as Ship } from "@/app/models/product";
+import AddTag from "./addtag";
 
 
 const Layout = () => {
     
     const [selectedImages, setSelectedImages] = useState <Array<string>> ([])
-    const [ProductInfo, setProductInfo] = useState <Product>()
-    const [shipping, setShipping] = useState()
-    const [sales, setSales] = useState <Sales>()
+    const [ProductInfo, setProductInfo] = useState <Product> ()
+    const [shipping, setShipping] = useState <Ship> ()
+    const [sales, setSales] = useState <Sales> ()
 
     const handleProductInfoCallback = (product: Product) => {
         setProductInfo(product);
@@ -25,38 +24,30 @@ const Layout = () => {
     const handleMediaCallback = ( images: Array<string> )=>{
         setSelectedImages(images);
     }
-    const handleShippingCallback = () =>{
-        
+    const handleShippingCallback = (ship: Ship) =>{
+        setShipping(ship)
     }
     const handleSalesInfoCallback = (sales: Sales) =>{
         setSales(sales);
     }
-    
-
 
 
     return ( 
         <div>
         <div className="scroll-smooth mt-12 pr- h-50 grid grid-cols-3 gap-4 content-center  ">
-<<<<<<< HEAD
-            <div className="ps-8 mb-3 ml-5 text-gray-500 dark:text-gray-400">
-                <div className="font-semibold text-xl">Product Information</div>
-                <ProductInformation callback={handleProductInfoCallback}/>
-=======
             <div className="ps-8 mb-3 ml-5 text-gray-900 dark:text-black-950">
                 <div className="font-semibold text-xl ml-6">Product Information</div>
-                <ProductInformation/>
->>>>>>> b57bbf25aadfc7000cb787c5eba2e2e488abbc29
+                <ProductInformation callback={handleProductInfoCallback}/>
             </div>
 
-            <div className="ps-8 mb-3 ml-9 text-gray-900 dark:text-gray-400">
+            <div className="ps-8 mb-3 ml-5 text-gray-500 dark:text-gray-400">
                 <div className="font-semibold text-xl">Sales Information</div>
                 <SalesInformation callback={handleSalesInfoCallback}/>
             </div>
 
             <div className="ps-8 mb-3 text-gray-900 dark:text-gray-400">
                 <div className="font-semibold text-xl ml-6">Shipping</div>
-                <Shipping/>
+                <Shipping callback={handleShippingCallback}/>
             </div>
         </div>
 
@@ -75,11 +66,6 @@ const Layout = () => {
             <TagSection/>
             
             </div>
-
-        </div>
-
-        <div>
-            <SubmitButton/>
         </div>
 
         </div>
